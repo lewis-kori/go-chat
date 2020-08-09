6f63046282cc9cbe364a7b54f79a64bdb9ae27cc
lewis-kori <lewiskosh@gmail.com>
2020-08-09 13:12:40 +0300

    complete code

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/lewis-kori/realtime-chat/pkg/websocket"
+)
+
+// define our WebSocket endpoint
+func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
+
+	// upgrade this connection to a WebSocket
+	// connection
+	conn, err := websocket.Upgrade(w, r)
+	if err != nil {
+		fmt.Fprintf(w, "%+v\n", err)
+	}
+
+	client := &websocket.Client{
+		Conn: conn,
+		Pool: pool,
+	}
+
+	pool.Register <- client
+	client.Read()
+}
+
+func setupRoutes() {
+	pool := websocket.NewPool()
+	go pool.Start()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Simple server")
+	})
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(pool, w, r)
+	})
+}
+
+func main() {
+	setupRoutes()
+	http.ListenAndServe(":8090", nil)
+}
